b8b1ca070e0589df2e2e4c351d73a0f2f1271948
ellathorburn <99ellathor@gmail.com>
2025-09-03 16:20:23 +0200

    building practice app

--- practice/index.php
+++ practice/index.php
@@ -3,11 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+            body {
+                display: grid;
+                place-items: center;
+                margin: 0;
+                font-family: sans-serif;
+            }
+    </style>
 </head>
 <body>
+<?php
+$books = [
+        "Harry Potter",
+    "Percy Jackson",
+    "1984"
+];
+
+?>
 <h1>
+Recommended Books
+</h1>
 
-
-    </h1>
+<ul>
+    <?php foreach ($books as $book): ?>
+        <li><?= "You have read " . $book ?> </li>
+    <?php endforeach; ?>
+</ul>
 </body>
 </html>
